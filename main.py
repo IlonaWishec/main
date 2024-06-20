403c32a4047ae12eba80f1998600251548d77d2a
IlonaWishec <ilonawishec.management@gmail.com>
2024-06-20 13:51:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+print(len(a)) #1
+
+first = 1
+second = 2
+summa = first+second
+diff = first-second
+print(summa, diff) #3, -1
+
+first = 1
+second = 2
+third = 3
+mean = (first+second+third)/3
+print(mean) # 2.0
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string+', '+first_string) # Понедельник, Вторник
+
+a = 1
+b = 2
+c = 3
+f = (a*b)+(a*c)
+print((f**3)/2) #62.5
